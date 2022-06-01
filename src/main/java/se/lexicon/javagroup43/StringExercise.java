c61b16b2b60a969adaf818b830384b9cfbae6be5
Jack Ninway <jack.ninway@gmail.com>
2022-06-01 15:09:03 +0200

    Leap

---
+++ src/main/java/se/lexicon/javagroup43/StringExercise.java
@@ -0,0 +1,17 @@
+package se.lexicon.javagroup43;
+
+import java.util.Scanner;
+
+public class StringExercise
+{
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.println("What is your name?" );
+        name = scanner.nextLine();  // Read user input
+        System.out.println("Hello " + name);  // Output user input
+    }
+
+}
