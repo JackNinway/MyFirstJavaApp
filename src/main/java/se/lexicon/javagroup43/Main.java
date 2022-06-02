ea90cb23bb6d34944a38acbb8c8c1c8330572849
Jack Ninway <jack.ninway@gmail.com>
2022-06-02 13:08:25 +0200

    NewRowByEnterChar

--- src/main/java/se/lexicon/javagroup43/Main.java
+++ src/main/java/se/lexicon/javagroup43/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello"); //
-        System.out.println("Jack");
+        System.out.print("Hello" +'\n' +"Jack"); //
+//        System.out.println("Jack");
     }
 }
