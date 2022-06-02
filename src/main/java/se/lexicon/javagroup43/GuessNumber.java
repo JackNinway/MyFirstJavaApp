baf6322f5d097ba97a03c61a80875b820a1a568c
Jack Ninway <jack.ninway@gmail.com>
2022-06-02 15:42:10 +0200

    GuessNr

---
+++ src/main/java/se/lexicon/javagroup43/GuessNumber.java
@@ -0,0 +1,33 @@
+package se.lexicon.javagroup43;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int ranNr =1, inputNr = 3;
+        int nGuess=0;
+
+        while (ranNr != inputNr)
+        {
+            ranNr = rand.nextInt(500);
+            ranNr += 1;
+            System.out.println("Enter a number between 1 and 500" );
+            inputNr = scanner.nextInt();
+            nGuess++;
+            if (inputNr==ranNr)
+                System.out.println("You guess was correct at the " + nGuess + "trial");
+            else if ( inputNr>ranNr )
+            {
+                System.out.println("You guess was to high, guess nr:" + nGuess);
+            }
+            else
+                System.out.println("You guess was to low, guess nr:" + nGuess);
+            System.out.println("Correct nr: " +ranNr );
+
+        }
+    }
+}
