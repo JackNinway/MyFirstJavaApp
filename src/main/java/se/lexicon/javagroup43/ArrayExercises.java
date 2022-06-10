9b5f426d2cbb324fbbeb5178beb28314e243873d
Jack Ninway <jack.ninway@gmail.com>
2022-06-10 15:15:51 +0200

    RemoveDuplicate with sorting

--- src/main/java/se/lexicon/javagroup43/ArrayExercises.java
+++ src/main/java/se/lexicon/javagroup43/ArrayExercises.java
@@ -1,7 +1,43 @@
 package se.lexicon.javagroup43;
 import java.util.Arrays;
-
+import java.util.Formatter;
 public class ArrayExercises {
+
+public static void RemoveDuplicate() {
+    int[] aN1 = {20, 20, 40, 20, 30, 40, 50, 60, 50};
+    Arrays.sort(aN1);
+    int[] aCleaned = new int[aN1.length];
+    int j=0;
+    for (int i = 0; i < aN1.length - 1; i++) {
+        if (aN1[i] != aN1[i + 1]) {
+            aCleaned[j++] = aN1[i];
+        }
+    }
+    aCleaned[j++] = aN1[aN1.length - 1];
+    System.out.print("     \n Removing duplicates \n   ");
+
+    for (int i = 0; i < j; i++)
+        System.out.print(aCleaned[i] + " ");
+
+}
+//Average of values in an array
+public static void PrintUnEven(){
+        int aN1 []  ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("       Odd numbers:       " );
+        for(int i=0; i< aN1.length; i++) {
+            if((aN1[i] % 2) != 0)
+               System.out.print(" " + aN1[i]);
+        }
+    }
+//Average of values in an array
+public static void AverageOfArray(){
+    int aN1 []  ={43, 5, 23, 17, 2, 14};
+    float  average=0, sum=0;
+    for(int i=0; i< aN1.length; i++)
+       sum = sum + aN1[i];
+    average = sum / aN1.length;
+    System.out.printf("\n The average is:   %.1f\n", average);
+}
 
 // 2 dimensional string array
     public static void TwoDStringArray()   {
@@ -57,6 +93,9 @@ public class ArrayExercises {
         SortStringArray();
         CopyElements();
         TwoDStringArray();
+        AverageOfArray();
+        PrintUnEven();
+        RemoveDuplicate();
     }
 }
 
